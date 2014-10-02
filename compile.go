8765bb93c578598b55d56304b44addcaab4cbd6e
maemual <maemual@gmail.com>
2014-10-02 10:50:29 +0800

    基本完成代码编译功能

--- compile.go
+++ compile.go
@@ -8,42 +8,67 @@ import (
 	"time"
 )
 
-func compile(codepath, lang string) (compileOutput string, err error) {
+func compile(codepath, lang string) (string, error) {
+
+	cmd := new(exec.Cmd)
+
+	switch lang {
+	case "cpp":
+		cmd = exec.Command(
+			"g++",
+			codepath,
+			"-o", "Main",
+			"-static", "-w",
+			"-lm", "-O2",
+			"-DONLINE_JUDGE",
+		)
+	case "c":
+		cmd = exec.Command(
+			"gcc",
+			codepath,
+			"-o", "Main",
+			"-static", "-w",
+			"-lm", "-std=c99",
+			"-O2", "-DONLINE_JUDGE",
+		)
+	case "java":
+		cmd = exec.Command(
+			"javac",
+			codepath,
+			"-d", ".",
+		)
+	}
+
 	ch := make(chan string)
-	compile := func(cmd *exec.Cmd) {
+	e := make(chan bool)
+
+	go func(cmd *exec.Cmd) {
 		stderr, err := cmd.StderrPipe()
 		if err != nil {
 			log.Printf("Error: %s\n", err)
+			e <- true
 			return
 		}
 		if err = cmd.Start(); err != nil {
 			log.Printf("Error: %s\n", err)
+			e <- true
 			return
 		}
 		bytes, _ := ioutil.ReadAll(stderr)
 		if err := cmd.Wait(); err != nil {
 			log.Printf("Error: %s\n", err)
+			e <- true
 			return
 		}
 		ch <- string(bytes)
 		return
-	}
-
-	switch lang {
-	case "cpp":
-		cmd := exec.Command("g++", codepath)
-		go compile(cmd)
-	case "c":
-		cmd := exec.Command("gcc", codepath)
-		go compile(cmd)
-	case "java":
-		cmd := exec.Command("javac", codepath)
-		go compile(cmd)
-	}
+	}(cmd)
 
 	select {
 	case res := <-ch:
 		return res, nil
+	case <-e:
+		return "", errors.New("System Error")
 	case <-time.After(time.Second * 15):
 		return "", errors.New("Compile Time Out")
 	}
